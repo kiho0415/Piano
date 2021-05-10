a505e8ddc10963b451c4408a0362e14899cac689
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2021-05-10 19:29:49 +0900

    Initial Commit

---
+++ Piano/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piano
+//
+//  Created by 森田貴帆 on 2021/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
